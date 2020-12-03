bae72a7c9ec4174cd0f119e2f0fdaf3226af7340
Gilles Margerin <gillesmargerin@gmail.com>
2020-12-03 19:26:04 +0100

    file upload checks code improvements, added file size check

--- components/Form.jsx
+++ components/Form.jsx
@@ -2,32 +2,15 @@ function Form(props) {
   const language = props.language
   const fileInput = () => document.querySelector('#file') 
   const fileList = () => document.querySelector('.file-list-container')
-  let userFiles = []
+  const userFiles = []
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    const formData = new FormData()
-    const initialForm = document.querySelector('#contact-data')
-
-    for (let i = 1; i < 5; i++) {
-      formData.append(`${initialForm.elements[i].name}`, `${initialForm.elements[i].value}`)
+  const handleFiles = () => {
+    const fileChecks = {
+      type: true,
+      size: true,
+      list: true
     }
-    
-    for (let i = 0; i < userFiles.length; i++) {
-      formData.append(`file-${i}`, userFiles[i])
-    }
-    
-    await fetch('http://localhost:3000/contact', {
-      method: 'POST',
-      body: formData
-    })
-    
-    initialForm.reset()
-    fileList().innerHTML = ''
-    userFiles.length = 0
-  }
 
-  const fileCheck = () => {
     const authorizedFiles = [
       "text/plain",
       "application/rtf",
@@ -37,28 +20,28 @@ function Form(props) {
       "application/vnd.oasis.opendocument.text",
       "application/vnd.oasis.opendocument.presentation"
     ]
-    
-    if (fileInput().files.length > 5) {
-      fileInput().value = '';
-      return alert('Too many files')
-    } 
+
+    if (userFiles.length + fileInput().files.length > 5) {
+      fileInput().value='';
+      fileChecks.list = false
+      return alert('Too many files, please upload a maximum of 5 files.')
+    }
 
     for (file of fileInput().files) {
-      !authorizedFiles.find(elem => elem === file.type) && function() {
+      if (file.size > 1048576) {
         fileInput().value='';
-        return alert('Wrong file type');
+        fileChecks.size = false
+        return alert(`File ${file.name} is too big, file size must be below 10Mb.`)
+      }
+
+      !authorizedFiles.find(typeCheck => typeCheck === file.type) && function() {
+        fileInput().value='';
+        fileChecks.type = false
+        return alert('Wrong file type, please upload one of the following file type: .txt, .rtf, .doc, .docx, .pdf, .odt,.odp');
       }()
     }
-  }
-
-  const handleFiles = () => {
-    if (userFiles.length + fileInput().files.length > 5) {
-      return alert('Too many files')
-    } else {
-      userFiles.push(...Array.from(fileInput().files))
-    }
-
-    fileCheck()
+    
+    if (!fileChecks.type  || !fileChecks.size || !fileChecks.list) return
 
     for (let i = 0, j = fileInput().files.length; i < j; i++) {
       const file = fileInput().files[i]
@@ -83,6 +66,29 @@ function Form(props) {
       fileList().append(div)
       div.append(description, singleFile, elementButton)
     };
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const formData = new FormData()
+    const initialForm = document.querySelector('#contact-data')
+
+    for (let i = 1; i < 5; i++) {
+      formData.append(`${initialForm.elements[i].name}`, `${initialForm.elements[i].value}`)
+    }
+    
+    for (let i = 0; i < userFiles.length; i++) {
+      formData.append(`file-${i}`, userFiles[i])
+    }
+    
+    await fetch('http://localhost:3000/contact', {
+      method: 'POST',
+      body: formData
+    })
+
+    initialForm.reset()
+    fileList().innerHTML = ''
+    userFiles.length = 0
   }
 
   return(
